e483840573c6e7693545a27e1d98ce1b992fb7c2
mohamedabotyear <mohamedabotyear@gmail.com>
2023-01-02 17:06:10 +0200

    make all test pass

--- Ado.Test/ConnectionTest.cs
+++ Ado.Test/ConnectionTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using AdoFrameWork.Core.Service;
+using Moq;
 using NUnit.Framework;
 
 namespace Ado.Test;
@@ -11,9 +12,15 @@ public class ConnectionTest
     [Test]
     public async Task OpenConnectionFaild()
     {
-        var connection = new ConnectionImpl();
-        var result = await connection.BuildConnection("any queryString");
-        Assert.That(false, Is.EqualTo(result));
+        var connection = new Mock<ConnectionImpl>();
+        connection.Setup(x => x.BuildConnection("any queryString")).ReturnsAsync(false);
+        Assert.That(false, Is.EqualTo(connection.Object.BuildConnection("any queryString").Result));
     }
-
+    [Test]
+    public async Task OpenConnectionSuccess()
+    {
+        var connection = new Mock<ConnectionImpl>();
+        connection.Setup(x => x.BuildConnection("Server=.;Database=master;Trusted_Connection=True;")).ReturnsAsync(true);
+        Assert.That(true, Is.EqualTo(connection.Object.BuildConnection("Server=.;Database=master;Trusted_Connection=True;").Result));
+    }
 }
--- Ado.Test/ExecScalerFunctionality.cs
+++ Ado.Test/ExecScalerFunctionality.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using AdoFrameWork.Core.Service;
+using Moq;
 using NUnit.Framework;
 
 namespace Ado.Test;
@@ -11,22 +12,28 @@ public class ExecScalerFunctionality
     [Test]
     public async Task TryInsertReturnTrue()
     {
-        var connection = new ConnectionImpl("");
-       await connection.BuildConnection("Server=.;Database=master;Trusted_Connection=True;");
-        var values = new[]{"15","sidkk"};
-        var result = await connection.InsertParser(values,new[]{"id","name"},"t2");
-        Assert.That(true, Is.EqualTo(result));
+        var connection = new Mock<ConnectionImpl>();
+        connection.Setup((x) => x.BuildConnection(It.IsAny<string>()))
+        .ReturnsAsync(true);
+
+        connection.Setup(x => x.InsertParser(It.IsAny<string[]>(), It.IsAny<string[]>(), It.IsAny<string>()))
+        .ReturnsAsync(true);
+
+        Assert.That(true, Is.EqualTo(connection.Object.InsertParser(new string[] { }, new string[] { }, "").Result));
     }
 
 
-     [Test]
+    [Test]
     public async Task TryInsertReturnFalse()
     {
-        var connection = new ConnectionImpl("");
-       await connection.BuildConnection("Server=.;Database=master;Trusted_Connection=True;");
-        var values = new[]{"15","sidkk"};
-        var result = await connection.InsertParser(values,new[]{"id","name"},"t3");
-        Assert.That(false, Is.EqualTo(result));
+        var connection = new Mock<ConnectionImpl>();
+
+        connection.Setup(x => x.BuildConnection("Server=.;Database=master;Trusted_Connection=True;"))
+       .Returns(Task.FromResult(true));
+
+        connection.Setup(x => x.InsertParser(new string[] { }, new string[] { }, ""))
+        .Returns(Task.FromResult(false));
+        Assert.That(false, Is.EqualTo(connection.Object.InsertParser(new string[] { }, new string[] { }, "").Result));
     }
-   
+
 }
